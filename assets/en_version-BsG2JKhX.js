68dc28c5eff81cb2f7b69246801ccea46c94b884
m6rshm3ll0w <mihail.gm.kms@gmail.com>
2025-09-15 17:38:23 +0300

    Deploy 2025-09-15 17:38:23

---
+++ assets/en_version-BsG2JKhX.js
@@ -0,0 +1,8 @@
+const n=`---\r
+title: "About this mini-blog"\r
+description: "This is my mini-blog where you can get information about me."\r
+date: 2025-09-14\r
+---\r
+\r
+Welcome to my mini-blog! Here you can find info about me and my projects...\r
+`;export{n as default};
